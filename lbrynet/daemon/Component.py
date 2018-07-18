ad7c8c400673aa96fb605826b5a07ce7092b02c2
hackrush <atulshyan96@gmail.com>
2018-07-19 00:43:42 +0530

    Fix metaclass not registering objects

--- lbrynet/daemon/Component.py
+++ lbrynet/daemon/Component.py
@@ -13,7 +13,7 @@ class ComponentType(type):
         return klass
 
 
-class Component(object):
+class Component(object, metaclass=ComponentType):
     """
     lbrynet-daemon component helper
 
@@ -21,7 +21,6 @@ class Component(object):
     methods
     """
 
-    __metaclass__ = ComponentType
     depends_on = []
     component_name = None
 
